22ef8921f639c7642e44b5ce5a63750fdb3e561d
Sierra Heins <sierra.heins@evergy.com>
2022-05-23 11:54:27 -0500

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -1,14 +1,40 @@
-import typer
-from ssg.site import Site
+import sys
 
-import ssg.parsers
+from pathlib import Path
 
-def main(source="content", dest="dist"):
-    config = {
-        "source": source,
-        "dest": dest,
-        "parsers":[ssg.parsers.ResourceParser(),],
-    }
-    Site(**config).build()
 
-typer.run(main)
+class Site:
+    def __init__(self, source, dest, parsers=None):
+        self.source = Path(source)
+        self.dest = Path(dest)
+        self.parsers = parsers or []
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            self.error(
+                "No parser for the {} extension, file skipped!".format(path.suffix)
+            )
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
